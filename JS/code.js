0d348bddde582de23cc75a62f0f30c251ab3288e
JorgeMont <monterrosaspix@gmail.com>
2023-01-20 21:58:44 -0600

    Homepage done and consuming data

--- JS/code.js
+++ JS/code.js
@@ -1,10 +1,39 @@
-const cardsContainer = document.querySelector('#cardsContainer');
 const apiUrl = 'https://rickandmortyapi.com/api/character';
+
+//Creando elementos
+const cardsContainer = document.querySelector('#cardsContainer');
+
 let algo;
 
 const printList = (datos) => {
     //funcionalidad
     console.log(datos);
+    datos.forEach(
+            (characterData) => {
+                //Crear un elemento
+                // const prueba = document.createElement('p');
+                const cardElement = document.createElement('article');
+                const imageElement = document.createElement('img');
+                const nameLabel = document.createElement('h1');
+                const anchorElement = document.createElement('a');
+
+                //Ordenando el elemento
+                cardElement.appendChild(anchorElement);
+                anchorElement.appendChild(nameLabel);
+                anchorElement.appendChild(imageElement);
+
+                //Agregar clases
+                cardElement.classList.add('characterCard');
+
+                nameLabel.textContent = characterData.name;
+                imageElement.src = characterData.image;
+
+                //Agregar su enlace
+                anchorElement.href = `/personaje?id=${characterData.id}`;
+                //Agregarlo al DOM
+                cardsContainer.appendChild(cardElement);
+            }
+        );
 }
 
 const getData = async(api) => {
